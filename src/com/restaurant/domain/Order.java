3b86cf78af6e89afbd3ab805069e611ff1310564
Moaid Hashem <Gamestorrent2015@gmail.com>
2025-11-10 13:17:07 +0200

    Setting up

---
+++ src/com/restaurant/domain/Order.java
@@ -0,0 +1,5 @@
+package com.restaurant.domain;
+
+public class Order {
+
+}
---
+++ src/com/restaurant/ui/Main.java
@@ -0,0 +1,10 @@
+package com.restaurant.ui;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=== Welcome to the Restaurant Ordering System ===");
+
+        System.out.println("\nThank you for visiting!");
+    }
+}
